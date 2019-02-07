e27f898cb4171ccd9ec2d302bb068b7b30c44070
SaLaaR HuSyN <shsallu35@gmail.com>
2019-02-07 23:24:56 +0500

    Initial commit

---
+++ DragDropInPictureBox/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragDropInPictureBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.AllowDrop = true;
+        }
+
+        private void pictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+        Image img;
+        private void pictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            foreach (string pic in ((string[])e.Data.GetData(DataFormats.FileDrop)))
+            {
+                img = Image.FromFile(pic);
+                pictureBox1.Image = img;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Filter = "JPG(*.JPG)|*.jpg";
+
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                img.Save(sf.FileName);
+            }
+        }
+    }
+}
